93369323594ef5866c303cc899db0260d4cba27e
Bhoomika <bhoomikahm18@gmail.com>
2023-10-12 14:26:02 +0530

    Setting timer for user input

--- js/script.js
+++ js/script.js
@@ -4,9 +4,22 @@ inputField = document.querySelector("input"),
 refreshBtn = document.querySelector(".refresh-word"),
 checkBtn = document.querySelector(".check-word");
 
-let correctWord;
+let correctWord,timer;
+
+const initTimer = maxTime => {
+    clearInterval(timer);
+    timer = setInterval(() => {
+        if(maxTime > 0) {
+            maxTime--;
+            return timeText.innerText = maxTime;
+        }
+        alert(`Time off! ${correctWord.toUpperCase()} was the correct word`);
+        initGame();
+    }, 1000);
+}
 
 const initGame = () => {
+    initTimer(30);
     let randomObj = words[Math.floor(Math.random() * words.length)]; //Getting random object from words
     let wordArray = randomObj.word.split(""); //Splitting each letter of random word
     for (let i = wordArray.length - 1; i > 0; i--) {
@@ -19,7 +32,6 @@ const initGame = () => {
     correctWord = randomObj.word.toLowerCase(); // passing random word to correctWord
     inputField.value = "";
     inputField.setAttribute("maxlength", correctWord.length); //setting input maxlength attribute value to word length
-    console.log(randomObj);
 }
 initGame();
 
