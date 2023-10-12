c5a6cdf4f6b41c7da51a4fc77e1e3aed479c794c
Bhoomika <bhoomikahm18@gmail.com>
2023-10-12 14:03:55 +0530

    Function to check the word is correct or not

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const wordText = document.querySelector(".word"),
 hintText = document.querySelector(".hint span"),
-refreshBtn = document.querySelector(".refresh-word");
+inputField = document.querySelector("input"),
+refreshBtn = document.querySelector(".refresh-word"),
+checkBtn = document.querySelector(".check-word");
+
+let correctWord;
 
 const initGame = () => {
     let randomObj = words[Math.floor(Math.random() * words.length)]; //Getting random object from words
@@ -12,6 +16,21 @@ const initGame = () => {
     }
     wordText.innerText = wordArray.join(""); //passing shuffled word as word text
     hintText.innerText = randomObj.hint; //passing random object hint as hint text
+    correctWord = randomObj.word.toLowerCase(); // passing random word to correctWord
     console.log(randomObj);
 }
 initGame();
+
+const checkWord = () => {
+    let userWord = inputField.value.toLowerCase(); //getting user value
+    if(!userWord) return alert(`Please enter a word to check`); //If user didn't enter anything
+
+    //If user word does not match with the currect word
+    if(userWord !== correctWord) return alert(`Oops! ${userWord} is not currect word`);
+
+    //If above conditions are failed then show below sentence, i.e. if user word match with the currect word
+    alert(`Congrats! ${userWord.toUpperCase()} is a currect word`)
+}
+
+refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord);
