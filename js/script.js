4bc2ae42370d3e871c77533727f61efbf78e6bdc
Bhoomika <bhoomikahm18@gmail.com>
2023-10-12 13:50:00 +0530

    Calling init function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+const wordText = document.querySelector(".word"),
+hintText = document.querySelector(".hint span"),
+refreshBtn = document.querySelector(".refresh-word");
+
 const initGame = () => {
-    // let randomObj = words
+    let randomObj = words[Math.floor(Math.random() * words.length)]; //Getting random object from words
+    let wordArray = randomObj.word.split(""); //Splitting each letter of random word
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1)); //Getting random number
+        //Shuffling and swiping wordArray letters randomly
+        [wordArray[i], wordArray[j]] =  [wordArray[j], wordArray[i]]      
+    }
+    wordText.innerText = wordArray.join(""); //passing shuffled word as word text
+    hintText.innerText = randomObj.hint; //passing random object hint as hint text
+    console.log(randomObj);
 }
+initGame();
